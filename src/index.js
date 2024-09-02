225df7c83d81a37c2be081075d48c9d6a253ecb5
Zaid Alshamaa <zshamaa20@gmail.com>
2024-09-02 16:35:02 +0300

    routing method

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import UserProvider from './Pages/Context';
 import EmailProvider from './Pages/email';
 import MyStringProvider from './Pages/MyStringProvider';
